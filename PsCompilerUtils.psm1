8efcd5bc0938ca6e72a4a8b995b65ad5179e033b
MinimalEffort <90430937+MinimalEffort07@users.noreply.github.com>
2025-08-17 14:18:47 +1000

    Minimal compiler working

--- PsCompilerUtils.psm1
+++ PsCompilerUtils.psm1
@@ -1,19 +1,21 @@
-$Compilers = "clang++.exe", "clang-cl.exe", "cl.exe"
+$Compilers = "clang-cl.exe", "cl.exe"
 
 function Invoke-Compiler {
 
     param(
         [Parameter(Mandatory=$False)]
         [String]$Compiler,
-        [Parameter(Mandatory=$True, ParameterSetName="in", HelpMessage="Input files to compile")]
+        [Parameter(Mandatory=$True, HelpMessage="Input files to compile")]
         [String[]]$InputFiles,
-        [Parameter(Mandatory=$False, ParameterSetName="options", HelpMessage="Options to pass to compiler")]
+        [Parameter(Mandatory=$False, HelpMessage="Options to pass to compiler")]
         [String[]]$Options,
+        [Parameter(Mandatory=$False, HelpMessage="Output file")]
+        [String]$OutputFile,
         [Switch]$Help,
-        [Switch]$H 
+        [Switch]$H
     )
 
-    if ($Help || $H) {
+    if ($Help -or $H) {
         Write-Output "Hello from help"
         return
     }
@@ -39,5 +41,22 @@ function Invoke-Compiler {
     }
     Write-Output "Using compiler: $Compiler"
 
+    # NORMALISE OPTION PREFIXES
+    $NormalisedOptions = @()
+    foreach($opt in $Options) {
+        $NormalisedOptions.Add($opt.Replace("-", "/"))
+    }
+
+    # SUFFIX OUTFILE IS MISSING
+    if ( $OutputFile -and !$OutputFile.ToLower().Contains(".obj") -and !$OutputFile.ToLower().Contains(".exe") ) {
+
+        if ($NormalisedOptions.Contains("/c")) {
+            $OutputFile = "/Fo:" + $OutputFile + ".obj"
+        } else {
+            $OutputFile = "/Fe:" + $OutputFile + ".exe"
+        }
+    }
+
+    & "$Compiler" $InputFiles $OutputFile $Options
 }
 set-alias -Name compile -Value Invoke-Compiler
